6e485db1f145ebd06c3e636788dc252f67f898ab
Khen <kalbarico91@rouse.com>
2026-04-15 15:46:37 +0800

    ClientConsent to ClientRequest1

--- CommonLib1/Models/Client/ClientRequest.cs
+++ CommonLib1/Models/Client/ClientRequest.cs
@@ -1,4 +1,5 @@
-﻿using static CommonLib1.Models.Constants;
+﻿using CommonLib1.Models.PolicyForms;
+using static CommonLib1.Models.Constants;
 
 namespace CommonLib1.Models.Client;
 
@@ -6,5 +7,6 @@ public sealed class ClientRequest
 {
     public ClientInformation   ClientInformation { get; set; } = new();
     public List<ClientService> ClientServices    { get; set; } = [];
+    public ConsentModel        ClientConsent     { get; set; } = new();
     public ClientStatus        Status            { get; set; } = ClientStatus.Pending;
 }
